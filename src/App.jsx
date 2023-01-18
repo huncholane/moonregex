4c3747819478d6c11ea7f29bfe95ef486226ec28
huncholane <huncho@ghostsystems.io>
2023-01-17 17:05:23 -0800

    using effects

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import parseBuilder from "./builder";
 
 const placeholder = `Enter your variables here like below.
@@ -10,64 +10,71 @@ group1Delim:\\n
 \t\tvar4:\\d`;
 
 function App() {
-  const [state, setState] = useState({
-    valid: true,
-    build: [],
-  });
+  const [valid, setValid] = useState(false);
+  const [build, setBuild] = useState("");
+  const [edited, setEdited] = useState(false);
+  const [target, setTarget] = useState(null);
   const regexInput = document.querySelector(
     "#regex-app > div > div.QtZzw > div > div.AUc0W > div.rjodX > div.cO83v > div.h9z_E.T886D > div > div > div:nth-child(1) > textarea"
   );
   const regexValue = document.querySelector(
     "#regex-app > div > div.QtZzw > div > div.AUc0W > div.rjodX > div.cO83v > div.h9z_E.T886D > div > div > div.CodeMirror-scroll > div.CodeMirror-sizer > div > div > div > div.CodeMirror-code"
   );
+  const builder = (
+    <textarea
+      rows="10"
+      className={
+        "w-full p-2 rounded-sm font-re101-input text-input" +
+        (valid ? "" : " text-red-500")
+      }
+      placeholder={placeholder}
+      spellCheck="off"
+      onKeyDown={(e) => {
+        switch (e.key) {
+          case "Enter":
+            break;
+          case "Tab":
+            e.preventDefault();
+            const index = e.target.selectionStart;
+            e.target.value =
+              e.target.value.substring(0, index) +
+              "\t" +
+              e.target.value.substring(index);
+            e.target.selectionStart = e.target.selectionEnd = index + 1;
+            break;
+        }
+      }}
+      onChangeCapture={(e) => {
+        const parseBuild = parseBuilder(e.target.value);
+        if (!parseBuild) setValid(false);
+        else setValid(true);
+        setBuild(parseBuild);
+        setTarget(e.target);
+      }}
+    ></textarea>
+  );
+
+  const updateRegex = () => {
+    const length = regexValue.textContent.length;
+    for (let i = 0; i < length; i++)
+      regexInput.dispatchEvent(
+        new KeyboardEvent("keydown", {
+          key: "Backspace",
+          code: "Backspace",
+          keyCode: 8,
+        })
+      );
+    regexInput.value = build;
+    setTimeout(() => target?.focus(), 100);
+  };
+
+  useEffect(updateRegex, [build]);
 
   return (
     <div>
       <div className="w-full">
         <div className="w-full text-lg text-center">Build Variables</div>
-        <textarea
-          rows="10"
-          className={
-            "w-full p-2 rounded-sm font-re101-input text-input" +
-            (state.valid ? "" : " text-red-500")
-          }
-          placeholder={placeholder}
-          onKeyDown={(e) => {
-            switch (e.key) {
-              case "Enter":
-                break;
-              case "Tab":
-                e.preventDefault();
-                const index = e.target.selectionStart;
-                e.target.value =
-                  e.target.value.substring(0, index) +
-                  "\t" +
-                  e.target.value.substring(index);
-                e.target.selectionStart = e.target.selectionEnd = index + 1;
-                break;
-            }
-          }}
-          onChangeCapture={(e) => {
-            state.build = [];
-            setState({ ...state });
-            const parseBuild = parseBuilder(e.target.value);
-            console.log(parseBuild);
-            if (!parseBuild) setState({ valid: false });
-            else setState({ valid: true });
-            //regexInput.focus();
-            const length = regexValue.textContent.length;
-            for (let i = 0; i < length; i++)
-              regexInput.dispatchEvent(
-                new KeyboardEvent("keydown", {
-                  key: "Backspace",
-                  code: "Backspace",
-                  keyCode: 8,
-                })
-              );
-            regexInput.value = parseBuild;
-            setTimeout(() => e.target.focus(), 100);
-          }}
-        ></textarea>
+        {builder}
       </div>
     </div>
   );
